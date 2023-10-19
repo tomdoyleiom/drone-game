51a8a34c3ed7c45f907887ffed73ac4836ade70f
Tom Doyle <tom.doyle.im@gmail.com>
2023-10-19 13:13:52 +0100

    reduce the size of the main container so the grid fits well

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <NavBar />
-        <main className="m-auto min-w-[300px] max-w-7xl p-4">{children}</main>
+        <main className="m-auto min-w-[300px] max-w-5xl p-4">{children}</main>
       </body>
     </html>
   );
