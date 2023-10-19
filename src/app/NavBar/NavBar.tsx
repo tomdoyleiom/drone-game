6c433a2fa49c80c6e5e10c30d0dc92c63083a3fd
Tom Doyle <tom.doyle.im@gmail.com>
2023-10-19 16:09:11 +0100

    Update graphics

--- src/app/NavBar/NavBar.tsx
+++ src/app/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import droneIcon from "../../assets/drone.svg";
 
 export default async function NavBar() {
   return (
@@ -7,12 +8,7 @@ export default async function NavBar() {
       <div className="navbar m-auto hidden max-w-7xl flex-col gap-2 lg:block lg:flex-row">
         <div className="flex-1">
           <Link href="/" className="btn btn-ghost text-xl normal-case">
-            <Image
-              src="https://placehold.co/40x40"
-              width={40}
-              height={40}
-              alt="site logo"
-            />
+            <Image src={droneIcon} width={40} height={40} alt="site logo" />
             Toy Drone
           </Link>
         </div>
